30b993e9061fe37ce9da86d283b70eee25ddd7c4
armdong <armdong@163.com>
2016-08-09 13:13:12 +0800

    翻糖定制活动新增图片上传预览功能

--- assets/js/gaw-diy.js
+++ assets/js/gaw-diy.js
@@ -18,7 +18,9 @@
             $aSpan = $oForm.find('.attrs').find('>span'),
             $oRequire = $oForm.find('#txtRequire'),
             $oLastWord = $oForm.find('#lastWord'),
-            iMaxLen = 800;
+            iMaxLen = 800,
+            $aThumbImg = $oForm.find('.thumb-img'),
+            $aThumbClose = $aThumbImg.find('.thumb-close');
 
         // 种类、口味和颜色切换逻辑
         $.each($aSpan, function(i, oSpan) {
@@ -28,15 +30,30 @@
         });
 
         // 字数限制逻辑
-        $(document).on('keyup', function(e){
-        	var text = $oRequire.val(),
-        		iLen = text.length;
+        $(document).on('keyup', function(e) {
+            var text = $oRequire.val(),
+                iLen = text.length;
 
-        	if (iLen > iMaxLen) {
-        		$oRequire.val(text.substring(0, iMaxLen));
-        		iLen = iMaxLen;
-        	}
-        	$oLastWord.html(iMaxLen - iLen);
+            if (iLen > iMaxLen) {
+                $oRequire.val(text.substring(0, iMaxLen));
+                iLen = iMaxLen;
+            }
+            $oLastWord.html(iMaxLen - iLen);
+        });
+
+        // 鼠标悬浮在缩略图上显示删除、预览按钮
+        $aThumbImg.hover(function() {
+            $(this).find('.operate').slideDown();
+        }, function() {
+            $(this).find('.operate').slideUp();
+        });
+
+        // 删除上传图片
+        $aThumbClose.on('click', function(e) {
+            $(this).closest('.thumb-img').remove();
+
+            // 阻止事件冒泡
+            return false;
         });
     }
 
