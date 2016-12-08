8070ce10afaeef26b78bd66c4b89fe17befafb32
chenxj92 <842674122@qq.com>
2016-12-08 15:53:52 +0800

    首页改版

---
+++ assets/js/common-20161207.js
@@ -0,0 +1,272 @@
+ /* 导航栏公用js */
+ (function() {
+
+     $(function() {
+
+         var iBackTimer = null;
+         var iBackStop = true;
+         var clientH = document.documentElement.clientHeight;
+         var oSadeBar = document.getElementById('sidebar');
+         var oActBtn = document.getElementById('activity');
+         var oBackBtn = document.getElementById('backToTop');
+         var oCloseBtn = $('.btn-close');
+
+         // 导航菜单切换事件
+         fnNavSelect();
+         // 城市定位
+         fnCityLocate();
+         // 城市切换事件
+         fnCityChange();
+         // 侧边栏垂直居中问题
+         fnSidebar();
+         // 模拟下拉框
+         //fnMockSelector();
+         fnMockSelectorAsync();
+         // placeholder支持
+         fnPlaceholderSupport();
+
+         $(window).on('resize', function() {
+             fnSidebar();
+         });
+
+         window.onscroll = function() {
+             var scrollT = document.documentElement.scrollTop || document.body.scrollTop;
+             if (scrollT >= 710) {
+                oSadeBar.style.position = 'fixed';
+                oSadeBar.style.top = '0'; 
+				$(oActBtn).css({
+					'right':'60px',
+					'transition':'.5s'
+				});
+             } else {
+                oSadeBar.style.position = 'absolute';
+                oSadeBar.style.top = '710px';
+                $(oActBtn).css({
+					'right':'-140px',
+					'transition':'.5s'
+				});
+             }
+             if (scrollT >= clientH) {
+                 oBackBtn.style.display = 'block';
+             } else {
+                 oBackBtn.style.display = 'none';
+             }
+             if (!iBackStop) {
+                 clearInterval(iBackTimer);
+             }
+             iBackStop = false;
+         };
+         
+         // 关闭活动弹窗
+         oCloseBtn.on('click',function(){
+         	 $(this).hide();
+             $(this).closest('li').css({
+				'height':'0',
+				'transition':'.5s'
+			 });
+			 $(this).hide();
+         });
+
+         // 回到顶部
+         oBackBtn.onclick = function() {
+             iBackTimer = setInterval(function() {
+                 //获取滚动条距离顶部的高度
+                 var scrollT = document.documentElement.scrollTop || document.body.scrollTop;
+                 var iSpeed = Math.floor(-scrollT / 6);
+                 document.documentElement.scrollTop = document.body.scrollTop = scrollT + iSpeed;
+                 iBackStop = true;
+                 if (scrollT == 0) {
+                     clearInterval(iBackTimer);
+                 }
+             }, 30);
+         };
+
+     });
+
+     function fnNavSelect() {
+         var $oNav = $('#nav');
+         var $aNavLi = $oNav.find('.item');
+         $.each($aNavLi, function(i, ele) {
+             $(ele).hoverDelay({
+                 hoverDuring: 0, // 鼠标移入延时时间，单位ms
+                 outDuring: 200, // 鼠标移除延时时间，单位ms
+                 hoverEvent: function() { // 鼠标移入处理事件
+                     $(this).find('.sub-nav').slideDown();
+                 },
+                 outEvent: function() { // 鼠标移除处理事件
+                     $(this).find('.sub-nav').stop().slideUp();
+                 }
+             });
+         });
+     }
+
+     function fnCityLocate() {
+         var $oBtnLocate = $('.btn-locate');
+         var $oCityMask = $('.location-mask');
+         $oBtnLocate.on('click', function() {
+             if ($oCityMask.hasClass('hide')) {
+                 $oCityMask.removeClass('hide');
+             } else {
+                 $oCityMask.addClass('hide');
+             }
+         });
+         $oCityMask.on('mouseleave', function() {
+             var _this = $(this);
+             if (!_this.hasClass('hide')) {
+                 setTimeout(function() {
+                     _this.addClass('hide');
+                 }, 400);
+             }
+             return false;
+         });
+     }
+
+     function fnCityChange() {
+         var $oCities = $('.cities');
+         var $aCitiesLi = $oCities.find('li');
+         $.each($aCitiesLi, function(i, ele) {
+             $(ele).on('click', function() {
+                 var changeCity = delnull($(this).find("a").text());
+                 var curCity = delnull($(".btn-locate").text());
+                 if (changeCity != curCity) {
+                     $(this).addClass('active').siblings('li').removeClass('active');
+                     CommenConfirm("切换城市购物篮将被清空!", "javascript:fnChangeCityCode('" + $(this).find("a").attr("citycode") + "');", "", "javascript:fnCityClose();", "");
+                 }
+
+             });
+         });
+     }
+
+//   function fnSidebar() {
+//       var viewHeight = $(window).height();
+//       var $oSidebar = $('#sidebar');
+//       var $oSidebarWrapper = $oSidebar.find('.sidebar-wrapper');
+//       var iHeight = $oSidebarWrapper.height();
+//       var iTop = 0;
+//       if (viewHeight > iHeight) {
+//           iTop = (viewHeight - iHeight) / 2;
+//       }
+//       $oSidebarWrapper.css({
+//           'margin-top': iTop + 'px'
+//       });
+//   }
+     
+     function fnSidebar() {
+         var $oSidebar = $('#sidebar');
+         var $oSidebarWrapper = $oSidebar.find('.sidebar-wrapper');
+         $oSidebarWrapper.css({
+			'margin-top': '50px'
+         });
+     }
+
+     function fnMockSelectorAsync() {
+         $('body').delegate('.selector-val', 'click', function() {
+             var _this = $(this);
+             // 关闭其他的selector
+             var $aMockSelector = $('body').find('.mock-selector');
+             $.each($aMockSelector, function(i, ele) {
+                 $(ele).find('.selector-options').removeClass('active');
+             });
+             var $oUl = _this.siblings('.selector-options');
+             $oUl.addClass('active');
+             //$oUl.toggleClass('active');
+         });
+         $('body').delegate('.option-item', 'click', function() {
+             $(this).addClass('selected').siblings('.option-item').removeClass('selected');
+             var $oUl = $(this).closest('.selector-options');
+             $oUl.removeClass('active');
+             var $oP = $oUl.siblings('.selector-val');
+             $oP.html($(this).html());
+         });
+         // 下拉框失去焦点事件
+         $('body').on('blur', '.mock-selector', function(event) {
+             console.log('111');
+             event.preventDefault();
+             /* Act on the event */
+             $(this).find('.selector-options').removeClass('active');
+         });
+
+         // 如果事件源不是.mock-selector, 关闭所有mock-selector
+         $('body').on('click', function(event) {
+             //event.preventDefault();
+             /* Act on the event */
+
+             //console.log(event.target);
+             var $oMockSel = $(event.target).closest('.mock-selector');
+             var isMockSel = $oMockSel.length > 0 ? true : false;
+
+             if (!isMockSel) {
+                $('.mock-selector').find('.selector-options').removeClass('active');
+             }
+         });
+     }
+
+     function fnMockSelector() {
+         var arrUl = [];
+         var arrP = [];
+         var $aSelector = $('.mock-selector');
+         var $aSelectorP = $aSelector.find('.selector-val');
+         $.each($aSelectorP, function(i, ele) {
+             arrP.push($(ele));
+             var $oSelectorUl = $(ele).siblings('.selector-options');
+             arrUl.push($oSelectorUl);
+         });
+         $.each(arrP, function(i, ele) {
+             var _index = i;
+             var $this = $(ele);
+             var $oSelectorUl = arrUl[i];
+             var $aSelectorLi = $oSelectorUl.find('.option-item');
+             $this.on('click', function() {
+                 $.each(arrUl, function(index, elem) {
+                     if (index != _index) {
+                         $(elem).removeClass('active');
+                     }
+                 });
+                 if ($oSelectorUl.hasClass('active')) {
+                     $oSelectorUl.removeClass('active');
+                 } else {
+                     $oSelectorUl.addClass('active');
+                 }
+             });
+             $aSelectorLi.on('click', function() {
+                 var iVal = $(this).html();
+                 $this.html(iVal);
+                 $(this).addClass('selected').siblings('.option-item').removeClass('selected');
+                 $oSelectorUl.removeClass('active');
+             });
+         });
+     }
+
+     function fnPlaceholderSupport() {
+         $('input, textarea').placeholder({ customClass: 'my-placeholder' });
+     }
+
+ })();
+
+ //过滤空格和换行符
+ function delnull(content) {
+     return content.replace(/[\n]/ig, '').replace(/[ ]/ig, '');
+ }
+
+ function fnCityClose() {
+     $(".location-mask").addClass('hide');
+     $("#myalertId").hide();
+ }
+
+ function fnChangeCityCode(citycode) {
+     $.ajax({
+         url: "/WebPage/SaveInfo.aspx",
+         dataType: "json",
+         type: "post",
+         timeout: "10000",
+         async: false,
+         data: { "type": "_cityok", "_citycode": citycode },
+         success: function(data) {
+             location.href = '/';
+         },
+         error: function() {
+             CommenMsg("网络错误！");
+         }
+     });
+
+ }
---
+++ assets/js/index-20161207.js
@@ -0,0 +1,77 @@
+(function() {
+
+	$(function() {
+		// 首页轮播图
+		fnSwiper();
+		fnCityLocation();
+		// 公告侧边栏
+		fnNotice();
+	});
+
+	function fnSwiper() {
+		$('.swiper-container').each(function(index) {
+			var count = $(this).find(".swiper-slide").length;
+			if (count > 1) {
+				var $this = $(this).swiper({
+					pagination: '.swiper-pagination', //分页标记
+					loop: true, //循环
+					autoplay: 5000, //自动播放，不指定默认不播放，单位为ms
+					autoplayDisableOnInteraction: false,
+					speed: 1000,
+					paginationClickable: true //分页标记是否可点击
+				});
+				//向左翻页
+				$(this).find('.swiper-button-prev').on('click', function(e) {
+					e.preventDefault(); //阻止默认的
+					$this.swipePrev(); //前一个
+				});
+				//向右翻页
+				$(this).find('.swiper-button-next').on('click', function(e) {
+					e.preventDefault(); //阻止默认的
+					$this.swipeNext(); //后一个
+				});
+				$(this).hover(function() {
+					$this.stopAutoplay();
+				}, function() {
+					$this.startAutoplay();
+				});
+			} else {
+				$(this).swiper({
+					autoplay: false //自动播放，不指定默认不播放，单位为ms
+				});
+				$(this).find('.swiper-button-prev').hide();
+				$(this).find('.swiper-button-next').hide();
+			}
+		});
+	}
+	
+	function fnCityLocation(){
+		var $oMaskLocation = $('.mask-city-location');
+		var $oBtnOk = $oMaskLocation.find('.btn-location-ok');
+		$oBtnOk.on('click',function(){
+			$oMaskLocation.hide();
+		});
+	}
+	
+	function fnNotice(){
+		var $oNotice = $('#notice');
+		var $oNoticeHandle = $oNotice.find('.notice-handle');
+		$oNoticeHandle.on('click',function(){
+			var isActive = $(this).hasClass('active');
+			if(isActive){
+				$(this).removeClass('active');
+				$oNotice.css({
+					'left':'-122px',
+					'transition':'.5s'
+				});
+			}else{
+				$(this).addClass('active');
+				$oNotice.css({
+					'left':'0',
+					'transition':'.5s'
+				});
+			}
+		});
+	}
+
+})();
