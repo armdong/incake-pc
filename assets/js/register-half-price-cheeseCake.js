4a6a495dcbe0559433b896744013a56579a461ee
5572189 <1542510269@qq.com>
2017-02-08 17:54:09 +0800

    fixed

--- assets/js/register-half-price-cheeseCake.js
+++ assets/js/register-half-price-cheeseCake.js
@@ -5,7 +5,7 @@
     	$("#mask").fadeOut();
     });
 	
-    $(".btn-01,.btn-02,.btn-03,.btn-04").click(function(){
+    $(".btn-01,.btn-02,.btn-03,.btn-04,.banner-bottom").click(function(){
     	$("#mask").fadeIn();
     })
 
