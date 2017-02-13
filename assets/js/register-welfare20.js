d5ed115b729829b856417c44f356b12e662712c8
5572189 <1542510269@qq.com>
2017-02-13 16:56:20 +0800

    ..

---
+++ assets/js/register-welfare20.js
@@ -0,0 +1,13 @@
+(function() {
+
+    var flag = true;
+    $(".receive").click(function(){
+    	if(flag){
+	    	$(".register").css("display","none");
+	    	$(".register-complete").css("display","block");
+	    }
+    })
+    
+
+   
+})();
