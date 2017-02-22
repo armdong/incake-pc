7eb60310e5ee91527f592d429f8db019c5a1b7ec
5572189 <1542510269@qq.com>
2017-02-22 16:31:30 +0800

    Buy two, get one free

---
+++ assets/js/register-half-price-cheeseCake-new.js
@@ -0,0 +1,31 @@
+(function() {
+
+    $(function() {
+
+        var $oContainer = $('#cheeeseCake'),
+            $oFrmContainer = $oContainer.find('.frm-container'),
+            iFrmT = $oFrmContainer.offset().top,
+            iFrmH = $oFrmContainer.height(),
+            iViewH = $(window).height(),
+            disT = iFrmT - iViewH + iFrmH;
+
+        fnFrmScroll(disT, $oFrmContainer);
+
+        $(window).on('scroll', function() {
+            fnFrmScroll(disT, $oFrmContainer);
+        });
+
+    });
+
+    function fnFrmScroll(disT, $obj) {
+        var scrollT = document.documentElement.scrollTop || document.body.scrollTop;
+        if (scrollT <= disT) {
+            $obj.addClass('flt');
+        } else {
+            if ($obj.hasClass('flt')) {
+                $obj.removeClass('flt');
+            }
+        }
+    }
+
+})();
